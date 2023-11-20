aab788a2ba05a24202d4b2ca6ae65588626cf779
tmasjc <thomas.jcng@gmail.com>
2023-11-20 17:06:04 +0800

    Alter chat component
    from gr.interface to gr.blocks.

--- main.py
+++ main.py
@@ -4,4 +4,24 @@ import gradio as gr
 def chat_with_openai(message, history):
     return generate(message)
 
-gr.ChatInterface(chat_with_openai).launch()
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg     = gr.Textbox(lines=1)
+    clear   = gr.ClearButton([msg, chatbot])
+
+    def greetings():
+        bot_message = "Aloha"
+        return bot_message
+
+    def respond(message, chat_history):
+        if chat_history is None:
+            return "", "greetings"
+        else:
+            bot_message = generate(message)
+            chat_history.append((message, bot_message))
+            return "", chat_history
+    
+    # event trigger
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+
+demo.launch()
