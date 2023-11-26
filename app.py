2d0141a12f37b4d9ccbb879a5150b778d94c8d89
tmasjc <thomas.jcng@gmail.com>
2023-11-26 10:33:14 +0800

    Code refactoring

--- app.py
+++ app.py
@@ -4,21 +4,22 @@ from utils.database import ChatSession, add_session_record
 import panel as pn
 from bokeh.io import curdoc
 import json
+from mock_data import mock_session
 
 # initialize and configure Panel
 pn.extension("perspective")
 
-# manual set conversation session info
+# to get current panel document object
 doc = curdoc()
-chat_session = {
-    "bot":  {"name": "Bob", "avatar": "🤖"},
-    "user": {"name": "Tom", "avatar": "👨🏻"},
-}
+
+# conversation entities
+BOT  = mock_session["bot"].get("name", "bot")
+USER = mock_session["user"].get("name", "user")
 
 CURRENT_SESSION = ChatSession(
     chat_session_id=doc.session_context.id, 
-    bot_identifier=chat_session['bot']['name'], 
-    player_identifier=chat_session["user"]['name']
+    bot_identifier=BOT, 
+    player_identifier=USER
     )
 logger.trace(f"{CURRENT_SESSION}")
 
@@ -27,17 +28,12 @@ messages = [
     {"role": "system", "content": "You are a helpful assistant."},
 ]
 
-def insert_new_record():
-    if len(messages) == 1:
-        add_session_record(CURRENT_SESSION)
-        return True
-    return None
-
 # chat's callback func
 async def chat_fn(content: str, user: str, instance: pn.chat.ChatInterface):
     # only do once at the beginning
-    if insert_new_record():
-        logger.trace("Inserted new record.")
+    if len(messages) == 1:
+        add_session_record(CURRENT_SESSION)
+        logger.success("Successfuly inserted new record.")
 
     # stream output to screen
     final_output = ""
@@ -51,18 +47,13 @@ async def chat_fn(content: str, user: str, instance: pn.chat.ChatInterface):
     redis_client.set(doc.session_context.id, json.dumps(messages))
     logger.trace(messages)
 
-
-# build chat function
-BOT  = chat_session["bot"].get("name", "bot")
-USER = chat_session["user"].get("name", "user")
-
 chat_ui = pn.chat.ChatInterface(
     callback=chat_fn,
     callback_user=BOT,
     message_params=dict(
         default_avatars={
-            BOT:  chat_session["bot"]["avatar"],
-            USER: chat_session["user"]["avatar"],
+            BOT:  mock_session["bot"]["avatar"],
+            USER: mock_session["user"]["avatar"],
         },
     ),
 )
---
+++ mock_data.py
@@ -0,0 +1,20 @@
+import json
+
+with open('mock_users.json', 'r') as f:
+    """
+    Expected something like this,
+    {
+        "bot": {
+            "name": "XX",
+            "avatar": "url or emoji"
+        },
+        "user": {
+            "name": "YY",
+            "avatar": "url or emoji"
+        }
+    }
+    """
+    data = json.load(f)
+
+# export this
+mock_session: dict = data
