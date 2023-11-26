1d343d312bc222ee0870abbbda85f5c42acccd1d
tmasjc <thomas.jcng@gmail.com>
2023-11-26 18:23:02 +0800

    Done: Event driven summarization
    summarize conversation content upon session closing.

--- session_handler.py
+++ session_handler.py
@@ -1,4 +1,6 @@
 from utils.common import logger, redis_client
+from utils.utils import summarize
+from utils.database import ChatSession, Session
 
 # connection details
 pubsub  = redis_client.pubsub()
@@ -12,6 +14,15 @@ for message in pubsub.listen():
     if message['type'] == 'message':
         session_id = message['data'].decode()
         logger.success(f"Session id: {session_id}")
-        content = redis_client.get(session_id) 
-        if content: 
-            print(content) # do something with conversation content
+        raw_content = redis_client.get(session_id) 
+        if raw_content: 
+            summary = summarize(raw_content.decode())
+            logger.trace(f"Convo Summary: {summary[:300:]}...") # displays the first xx chars only
+            session = Session() # insert to database
+            record  = session.query(ChatSession).filter(ChatSession.chat_session_id == session_id).first()
+            if record:
+                record.summary = summary # update summary here
+                session.commit()
+            else:
+                logger.debug(f"No record {session_id} found.")
+            session.close()
--- utils/utils.py
+++ utils/utils.py
@@ -1,14 +1,29 @@
 from utils.common import config, logger
-from openai import AsyncOpenAI
+from openai import OpenAI, AsyncOpenAI
 
-client = AsyncOpenAI(api_key=config["OPENAI"]["api_key"])
+client = OpenAI(api_key=config["OPENAI"]["api_key"])
+async_client = AsyncOpenAI(api_key=config["OPENAI"]["api_key"])
+DEFAULT_MODEL = "gpt-3.5-turbo"
 
-async def generate(content: str, model: str = "gpt-3.5-turbo"):
-    completion = await client.chat.completions.create(
-        model=model, messages=content
-    )
+
+async def generate(content: str, model: str = DEFAULT_MODEL):
+    completion = await async_client.chat.completions.create(model=model, messages=content)
     result = completion.choices[0].message.content
     output = ""
     for char in result:
         output += char
         yield output
+
+
+def summarize(content: str, model: str = DEFAULT_MODEL):
+    prompt = [
+        {
+            "role": "system",
+            "content": "Your job is to summarize a conversation like a professional. You expect to receive a Python list. It contains a dialogue between 2 persons, 'assistant' and 'user'. Role `assistant` represents yourself and role `user` represents a friend. Please summarize the conversation into a gist. Answer using first-person tone of voice and use 'my friend' to represent the user. Be concise and direct.",
+        }
+    ]
+    prompt.append({"role": "user", "content": content})
+    # logger.trace(f"{prompt}")
+    response = client.chat.completions.create(model=model, messages=prompt, temperature=0)
+    result   = response.choices[0].message.content
+    return result
